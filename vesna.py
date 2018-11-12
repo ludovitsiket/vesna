78c0357feca90feea5eb871fb8a9a5f91acaa7dc
ludovitsiket <siket.ludovit@gmail.com>
2018-11-12 09:44:46 +0100

    Doplnenie alphabet o znaky a velke pismena.
    Try-except vo funkcii prevod().
    Funkcie abeceda(), spracovanie().
    Upravena funkcia hlavna().
    Skript akceptuje len 1 slovo ako argument.

--- vesna.py
+++ vesna.py
@@ -11,21 +11,32 @@ def prevod(rozlozene, alphabet):
     ciselna = []
     i = 0
     while i < len(rozlozene):
-      hodnota = alphabet.index(rozlozene[i])
-      ciselna.append(hodnota)
-      i = i+1
+        try:
+            hodnota = alphabet.index(rozlozene[i])
+            ciselna.append(hodnota)
+            i = i+1
+        except ValueError:
+            sys.exit()
     return ciselna
 
-def hlavna():
-    alphabet = ["a","á","ä","b","c","č","d",
-               "ď","dž","e","é","f","g",
-               "h","i","í","j","k","l",
-               "ĺ","ľ","m","n","ň","o",
-               "ó","ô","p","q","r","ŕ",
-               "s","š","t","ť","u","ú",
-               "v","w","q","x","y","ý",
-               "z","ž"]
- 
+def abeceda():
+    male_pismena = ["a","á","ä","b","c","č","d","ď","dž","e","é","f","g",
+               "h","i","í","j","k","l","ĺ","ľ","m","n","ň","o","ó","ô",
+               "p","q","r","ŕ","s","š","t","ť","u","ú","v","w","x",
+               "y","ý","z","ž"]
+
+    znaky = [",",".","!","?","_","-",";","/","=","*"]
+
+    velke_pismena = ["A","Á","B","C","Č","D","Ď","DŽ","E","É","F","G",
+               "H","I","Í","J","K","L","Ĺ","Ľ","M","N","Ň","O","Ó",
+               "P","Q","R","Ŕ","S","Š","T","Ť","U","Ú","V","W","X",
+               "Y","Ý","Z","Ž"]
+    alphabet = male_pismena + znaky + velke_pismena
+    return alphabet
+
+def spracovanie(abeceda):
+    # nezvlada znak medzery a hviezdicku na zaciatku
+    alphabet = abeceda()
     sprava = ''
     if len(sys.argv) != 2:
         sprava = 'prazdne'
@@ -34,5 +45,8 @@ def hlavna():
     rozlozene = rozklad_spravy(sprava)
     ciselna = prevod(rozlozene, alphabet)
     print(ciselna)
+
+def hlavna():
+    spracovanie(abeceda)
 
 hlavna()
