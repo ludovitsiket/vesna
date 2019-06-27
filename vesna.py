80d0491b636831a567abec3632b1512332170035
Ludo <siket.ludovit@gmail.com>
2019-06-27 09:51:22 +0200

    Cistejsi, krajsi kod.

--- vesna.py
+++ vesna.py
@@ -1,54 +1,60 @@
 #!/usr/bin/python3
 import sys
 
+
 def rozklad_spravy(sprava):
     upravene = []
     for znak in sprava:
         upravene.append(znak)
     return upravene
 
+
 def prevod(rozlozene, alphabet):
     ciselna = []
     for item in range(len(rozlozene)):
         try:
             hodnota = alphabet.index(rozlozene[item])
-            ciselna.append(hodnota+1)
+            ciselna.append(hodnota + 1)
         except ValueError as e:
             print(e)
             sys.exit()
     return ciselna
 
+
 def abeceda():
-    male_pismena = ["a","á","ä","b","c","č","d","ď","dž","e","é","f","g",
-               "h","i","í","j","k","l","ĺ","ľ","m","n","ň","o","ó","ô",
-               "p","q","r","ŕ","s","š","t","ť","u","ú","v","w","x",
-               "y","ý","z","ž"]
+    male_pismena = ["a", "á", "ä", "b", "c", "č", "d", "ď", "dž", "e", "é", "f", "g",
+                    "h", "i", "í", "j", "k", "l", "ĺ", "ľ", "m", "n", "ň", "o", "ó", "ô",
+                    "p", "q", "r", "ŕ", "s", "š", "t", "ť", "u", "ú", "v", "w", "x",
+                    "y", "ý", "z", "ž"]
 
-    znaky = [",",".","!","?","_","-",";","/","=","*",
-    "@","#","$","%","^","&", " ","+","0","1","2","3","4","5","6","7","8","9"]
+    znaky = [",", ".", "!", "?", "_", "-", ";", "/", "=", "*",
+             "@", "#", "$", "%", "^", "&", " ", "+", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-    velke_pismena = ["A","Á","B","C","Č","D","Ď","DŽ","E","É","F","G",
-               "H","I","Í","J","K","L","Ĺ","Ľ","M","N","Ň","O","Ó",
-               "P","Q","R","Ŕ","S","Š","T","Ť","U","Ú","V","W","X",
-               "Y","Ý","Z","Ž"]
+    velke_pismena = ["A", "Á", "B", "C", "Č", "D", "Ď", "DŽ", "E", "É", "F", "G",
+                     "H", "I", "Í", "J", "K", "L", "Ĺ", "Ľ", "M", "N", "Ň", "O", "Ó",
+                     "P", "Q", "R", "Ŕ", "S", "Š", "T", "Ť", "U", "Ú", "V", "W", "X",
+                     "Y", "Ý", "Z", "Ž"]
     alphabet = male_pismena + znaky + velke_pismena
     return alphabet
 
+
 def make_keys(alphabet):
-    x=1
-    y=[]
-    while x<(len(alphabet)+1):
+    x = 1
+    y = []
+    while x < (len(alphabet) + 1):
         y.append(x)
-        x+=1
+        x += 1
     return y
 
+
 def make_dict(alphabet, keys):
-    x=0
+    x = 0
     dictionary = dict.fromkeys(keys)
-    while x<(len(alphabet)):
-        dictionary[x] = alphabet[x-1]
-        x+=1
+    while x < (len(alphabet)):
+        dictionary[x] = alphabet[x - 1]
+        x += 1
     return dictionary
+
 
 def multi_replace(content, to_replace, repl):
     for elem in to_replace:
@@ -56,16 +62,19 @@ def multi_replace(content, to_replace, repl):
             content = content.replace(elem, repl)
     return content
 
+
 def citaj_subor(subor):
     with open(subor, 'r') as sprava:
         obsah = sprava.read()
-        obsah = multi_replace(obsah, ['\n','[',']'], "")
+        obsah = multi_replace(obsah, ['\n', '[', ']'], "")
     return obsah
+
 
 def zapis_subor(subor, obsah):
     with open(subor, 'w') as sprava:
         sprava.write(str(obsah))
     return sprava
+
 
 def help_syntax():
     print("Skript vyzaduje python3.x")
@@ -75,57 +84,64 @@ pre dekodovanie musi byt zakodovana sprava ulozena v subore kodovana.txt
 Parametre: 'k' zakodovanie suboru
            'd' dekodovanie suboru""")
 
+
 def coding(subor_spravy, alphabet, kod_sprava):
     sprava = citaj_subor(subor_spravy)
     rozlozene = rozklad_spravy(sprava)
     ciselna = prevod(rozlozene, alphabet)
-    kod = zapis_subor(kod_sprava, ciselna)
+    zapis_subor(kod_sprava, ciselna)
     print("OK")
 
+
 def slicing_list(content):
-    result=[]
-    a=0
-    b=3
-    character=','
-    dlzka = range(len(content)+b)
+    result = []
+    a = 0
+    b = 3
+    character = ','
+    dlzka = range(len(content) + b)
     while b in dlzka:
         decoded = (content[a:b])
         if character in decoded:
-             decoded = decoded.replace(character,'')
+            decoded = decoded.replace(character, '')
         result.append(decoded)
-        a+=3
-        b+=3
+        a += 3
+        b += 3
     return result
 
+
 def processing_decode(kod_sprava):
-    content=[]
+    content = []
     obsah = citaj_subor(kod_sprava)
     for item in obsah:
-        item = multi_replace(item, [' ','[',']'], "")
+        item = multi_replace(item, [' ', '[', ']'], "")
         content.append(item)
     content = ''.join(content)
     return content
+
 
 def decoding(kod_sprava, dictionary):
     content = processing_decode(kod_sprava)
     number_result = slicing_list(content)
     index = 0
-    while index<len(number_result):
-        for key in number_result:
-            key=str(number_result[index])
+    while index < len(number_result):
+        for _ in number_result:
+            key = str(number_result[index])
             print(dictionary[int(key)], end='')
-            index +=1
+            index += 1
     print('')
+
 
 def vynimka(e):
     print(e)
     help_syntax()
     sys.exit()
 
-def spracovanie(abeceda):
+
+def spracovanie():
     try:
         arg = sys.argv[1]
     except IndexError as e:
+        arg = False
         vynimka(e)
     alphabet = abeceda()
     keys = make_keys(alphabet)
@@ -146,8 +162,9 @@ def spracovanie(abeceda):
         help_syntax()
         sys.exit()
 
+
 if __name__ == '__main__':
     try:
-        spracovanie(abeceda)
+        spracovanie()
     except KeyboardInterrupt:
         sys.exit()
