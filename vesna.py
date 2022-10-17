823526fd7ba819751bf0f3560b9107a384c88221
ludo <siket.ludovit@gmail.com>
2022-10-17 09:18:51 +0200

    Complete translation to english.
    Minor edits, PEP 8.

--- vesna.py
+++ vesna.py
@@ -9,11 +9,11 @@ def decompose(value):
     return text
 
 
-def prevod(rozlozene, alphabet):
+def translation(decomp, alphabet):
     num = []
-    for item in range(len(rozlozene)):
+    for item in range(len(decomp)):
         try:
-            number_value = alphabet.index(rozlozene[item])
+            number_value = alphabet.index(decomp[item])
             num.append(number_value + 1)
         except ValueError as e:
             print(e)
@@ -22,20 +22,127 @@ def prevod(rozlozene, alphabet):
 
 
 def abc():
-    small_letters = ["a", "á", "ä", "b", "c", "č", "d", "ď", "dž", "e", "é", "f", "g",
-                    "h", "i", "í", "j", "k", "l", "ĺ", "ľ", "m", "n", "ň", "o", "ó", "ô",
-                    "p", "q", "r", "ŕ", "s", "š", "t", "ť", "u", "ú", "v", "w", "x",
-                    "y", "ý", "z", "ž"]
+    # slovak alphabet
+    small_letters = [
+        "a",
+        "á",
+        "ä",
+        "b",
+        "c",
+        "č",
+        "d",
+        "ď",
+        "dž",
+        "e",
+        "é",
+        "f",
+        "g",
+        "h",
+        "i",
+        "í",
+        "j",
+        "k",
+        "l",
+        "ĺ",
+        "ľ",
+        "m",
+        "n",
+        "ň",
+        "o",
+        "ó",
+        "ô",
+        "p",
+        "q",
+        "r",
+        "ŕ",
+        "s",
+        "š",
+        "t",
+        "ť",
+        "u",
+        "ú",
+        "v",
+        "w",
+        "x",
+        "y",
+        "ý",
+        "z",
+        "ž"]
 
-    chars = [",", ".", "!", "?", "_", "-", ";", "/", "=", "*",
-             "@", "#", "$", "%", "^", "&", " ", "+", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    chars = [
+        ",",
+        ".",
+        "!",
+        "?",
+        "_",
+        "-",
+        ";",
+        "/",
+        "=",
+        "*",
+        "@",
+        "#",
+        "$",
+        "%",
+        "^",
+        "&",
+        " ",
+        "+",
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9"]
 
-    big_letters = ["A", "Á", "B", "C", "Č", "D", "Ď", "DŽ", "E", "É", "F", "G",
-                     "H", "I", "Í", "J", "K", "L", "Ĺ", "Ľ", "M", "N", "Ň", "O", "Ó",
-                     "P", "Q", "R", "Ŕ", "S", "Š", "T", "Ť", "U", "Ú", "V", "W", "X",
-                     "Y", "Ý", "Z", "Ž"]
-    alphabet = small_letters + chars + big_letters
-    return alphabet
+    big_letters = [
+        "A",
+        "Á",
+        "B",
+        "C",
+        "Č",
+        "D",
+        "Ď",
+        "DŽ",
+        "E",
+        "É",
+        "F",
+        "G",
+        "H",
+        "I",
+        "Í",
+        "J",
+        "K",
+        "L",
+        "Ĺ",
+        "Ľ",
+        "M",
+        "N",
+        "Ň",
+        "O",
+        "Ó",
+        "P",
+        "Q",
+        "R",
+        "Ŕ",
+        "S",
+        "Š",
+        "T",
+        "Ť",
+        "U",
+        "Ú",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Ý",
+        "Z",
+        "Ž"]
+    return (small_letters + chars + big_letters)
 
 
 def make_keys(alphabet):
@@ -63,32 +170,32 @@ def multi_replace(content, to_replace, repl):
     return content
 
 
-def read_file(subor):
-    with open(subor, 'r') as msg:
+def read_file(some_file):
+    with open(some_file, 'r') as msg:
         con = msg.read()
         con = multi_replace(con, ['\n', '[', ']'], "")
     return con
 
 
-def write_file(subor, con):
-    with open(subor, 'w') as msg:
-         msg.write(str(con))
+def write_file(file_to_write, con):
+    with open(file_to_write, 'w') as msg:
+        msg.write(str(con))
     return msg
 
 
 def help_syntax():
-    print("Skript vyzaduje python3.x")
-    print("""Syntax: python3 vesna.py parameter
-Sprava urcena na zakodovanie musi byt ulozena v subore sprava.txt,
-pre dekodovanie musi byt zakodovana sprava ulozena v subore kodovana.txt
-Parametre: 'k' zakodovanie suboru
-           'd' dekodovanie suboru""")
+    print("Skcipt require python3.x")
+    print("""Syntax: python3 vesna.py parameters
+Message for coding must be in file sprava.txt,
+for decoding must be coded message in file kodovana.txt
+Parameters: 'k' for coding file
+           'd' for decoding file""")
 
 
 def coding(message_file, alphabet, coded_message):
-    sprava = read_file(message_file)
-    rozlozene = decompose(sprava)
-    num = prevod(rozlozene, alphabet)
+    msg = read_file(message_file)
+    decomp = decompose(msg)
+    num = translation(decomp, alphabet)
     write_file(coded_message, num)
     print("OK")
 
@@ -98,8 +205,8 @@ def slicing_list(content):
     a = 0
     b = 3
     character = ','
-    dlzka = range(len(content) + b)
-    while b in dlzka:
+    length = range(len(content) + b)
+    while b in length:
         decoded = (content[a:b])
         if character in decoded:
             decoded = decoded.replace(character, '')
@@ -143,7 +250,6 @@ def processing():
     except IndexError as e:
         arg = False
         exception(e)
-
     except SyntaxError as e:
         print(e)
         sys.exit()
@@ -162,7 +268,6 @@ def processing():
             sys.exit()
     except FileNotFoundError as e:
         exception(e)
-
 
 
 if __name__ == '__main__':
